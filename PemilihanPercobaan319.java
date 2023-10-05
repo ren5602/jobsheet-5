31b222c91eb50a22bb2ae865c9612741a527e05c
ren5602 <naziz5602@gmail.com>
2023-10-05 14:05:14 +0700

    percobaan 3

---
+++ PemilihanPercobaan319.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class PemilihanPercobaan319 {
+ public static void main(String[] args) {
+    Scanner input19 = new Scanner(System.in);
+    double angka1, angka2, hasil;
+    char operator;
+    System.out.print("Masukkan angka pertama: ");
+    angka1 = input19.nextDouble();
+    System.out.print("Masukkan angka kedua: ");
+    angka2 = input19.nextDouble();
+    System.out.print("Masukkan operatir (+ - * / ): ");
+    operator = input19.next().charAt(0);
+
+    switch (operator){
+        case '+':
+            hasil   = angka1 + angka2;
+            System.out.println(angka1 + " + " + angka2 + " = " + hasil);
+            break;
+        case '-':
+            hasil   = angka1 - angka2;
+            System.out.println(angka1 + " - " + angka2 + " = " + hasil);
+            break;
+        case '*':
+            hasil   = angka1 * angka2;
+            System.out.println(angka1 + " * " + angka2 + " = " + hasil);
+            break;
+        case '/':
+            hasil   = angka1 / angka2;
+            System.out.println(angka1 + " / " + angka2 + " = " + hasil);
+            break;
+    }
+}   
+}
